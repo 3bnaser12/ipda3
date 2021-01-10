97a895e7e9d03ac44e58c7cde95a63bfcbebf099
mohamed <mohdnaser54@yahoo.com>
2021-01-10 20:42:22 +0200

    first commit

---
+++ assests/js/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    $(".offer-carousel").owlCarousel({
+        rtl:true,
+        margin:10,
+        nav:true,
+        loop:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:true
+        }
+    }
+
+    });
+  });
+  $(document).ready(function(){
+    $(".customer-carousel").owlCarousel({
+        rtl:true,
+        margin:10,
+        nav:true,
+        loop:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:true
+        }
+    }
+
+    });
+  });
+  
